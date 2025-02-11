221d8cac62a0b2d5e46283d0dc8d0a7e6bd0097b
peeyush0928 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-02-11 13:09:24 +0530

    Time: 3 ms (64.14%) | Memory: 127.5 MB (25.37%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,7 +11,31 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        // First we are doing with two stack;
+        // First we are doing with two stack Time complexity of this code is O(N)
+        // if (head == nullptr) return false;
+        // ListNode* temp = head;
+        // stack<int> st1;
+        // while (temp != nullptr){
+        //     st1.push(temp->val);
+        //     temp = temp->next;
+        // }
+        // stack<int> st2;
+        // stack<int> tempst = st1;
+        // while (!tempst.empty()){
+        //     st2.push(tempst.top());
+        //     tempst.pop();
+        // }
+        // while (!st1.empty() && !st2.empty()){
+        //     if(st1.top() != st2.top()){
+        //         return false;
+        //     }
+        //     st1.pop();
+        //     st2.pop();
+        // }
+        // return true;
+    
+        // Now i am doing this code in single stack time complexity of this code is O(N)
+
         if (head == nullptr) return false;
         ListNode* temp = head;
         stack<int> st1;
@@ -19,18 +43,13 @@ public:
             st1.push(temp->val);
             temp = temp->next;
         }
-        stack<int> st2;
-        stack<int> tempst = st1;
-        while (!tempst.empty()){
-            st2.push(tempst.top());
-            tempst.pop();
-        }
-        while (!st1.empty() && !st2.empty()){
-            if(st1.top() != st2.top()){
+        temp = head;
+        while (!st1.empty() && temp != nullptr){
+            if (st1.top() != temp->val){
                 return false;
             }
             st1.pop();
-            st2.pop();
+            temp = temp->next;
         }
         return true;
     }

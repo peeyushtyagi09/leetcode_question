65a6854da1329b44b5f7d967428cf1655293dbb0
peeyush0928 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-02-16 15:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (63.66%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -13,25 +13,39 @@ public:
     ListNode* oddEvenList(ListNode* head) {
         // that is the Brute force solution of that question with (Extra space)
 
-        if (head == nullptr || head->next == nullptr) return head;
-        ListNode* oddHead = new ListNode(0), *odd = oddHead;
-        ListNode* evenHead = new ListNode(0), *even = evenHead;
-        ListNode* temp = head;
-        int index = 0;
-        while (temp != nullptr){
-            if (index % 2 == 1){
-                odd->next = temp;
-                odd = odd->next;
-            }else{
-                even->next = temp;
-                even = even->next;
-            }
-            temp = temp->next;
-            index++;
-        }
-        odd->next = nullptr;
-        even->next = oddHead->next;
-        return evenHead->next;
+        // if (head == nullptr || head->next == nullptr) return head;
+        // ListNode* oddHead = new ListNode(0), *odd = oddHead;
+        // ListNode* evenHead = new ListNode(0), *even = evenHead;
+        // ListNode* temp = head;
+        // int index = 0;
+        // while (temp != nullptr){
+        //     if (index % 2 == 1){
+        //         odd->next = temp;
+        //         odd = odd->next;
+        //     }else{
+        //         even->next = temp;
+        //         even = even->next;
+        //     }
+        //     temp = temp->next;
+        //     index++;
+        // }
+        // odd->next = nullptr;
+        // even->next = oddHead->next;
+        // return evenHead->next;
 
+        // but we have the optimal solution solution of that proble that have the time and space complexity of o(n)
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* headeven = even;
+
+        while(even != nullptr && even->next != nullptr){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = headeven;
+        return head;
     }
 };

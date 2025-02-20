7e62e760113eb6944129298f0613233e2d99301a
peeyush0928 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-02-20 18:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (27.92%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        while(true){
-        int n = s.length();
-        bool found = false;
-        for (int i = 0; i < n; i++){
-            if(isdigit(s[i])){
-                for (int j = i - 1; j >=  0; i--){
-                    if(isalpha(s[j])){
-                        s.erase(j, 1);
-                        s.erase(i - 1, 1);
-                        found = true;
-                        break;
-                    }
-                }
-                break;
+        // but the is brute olution it has time complexity of O(n2)
+        // while(true){
+        // int n = s.length();
+        // bool found = false;
+        // for (int i = 0; i < n; i++){
+        //     if(isdigit(s[i])){
+        //         for (int j = i - 1; j >=  0; i--){
+        //             if(isalpha(s[j])){
+        //                 s.erase(j, 1);
+        //                 s.erase(i - 1, 1);
+        //                 found = true;
+        //                 break;
+        //             }
+        //         }
+        //         break;
+        //     }
+        // }
+        // if (!found) break;
+        // }
+        // return s;
+
+        // That one is the optimal solution in this we are using an stack;
+        stack<char> st;
+        for (char ch : s){
+            if(isdigit(ch)){
+                if(!st.empty()) st.pop();
+            }else{
+                st.push(ch);
             }
         }
-        if (!found) break;
+        string str = "";
+        while(!st.empty()){
+            str += st.top();
+            st.pop();
         }
-        return s;
+         reverse(str.begin(), str.end());
+        return str;
     }
 };

98a2f3dab941846045074b69986ff0a4f02fe826
peeyush0928 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-02-21 22:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (68.53%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -6,17 +6,29 @@ public:
         // step2 : Then reverse that from 0 to the index of that first occurance
         // step3 : then return that reverse string + remmaning string
 
-        int index = -1;
-        for(int i = 0; i < word.length(); i++){
-            if (word[i] == ch){
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) return word;
-        string prefix = word.substr(0, index + 1);
-        reverse(prefix.begin(), prefix.end());
+        // int index = -1;
+        // for(int i = 0; i < word.length(); i++){
+        //     if (word[i] == ch){
+        //         index = i;
+        //         break;
+        //     }
+        // }
+        // if (index == -1) return word;
+        // string prefix = word.substr(0, index + 1);
+        // reverse(prefix.begin(), prefix.end());
 
-        return prefix + word.substr(index + 1);
+        // return prefix + word.substr(index + 1);
+
+        // optimal solution of that code: (Using two pointer approach)
+        int index = word.find(ch);
+        if (index == string::npos) return word;
+        int left = 0;
+        int right = index;
+        while(left < right){
+            swap(word[left], word[right]);
+                left++;
+                right--;
+            }
+            return word;
     }
 };

ea4f26919b5bce7eb15a87b6beebe9d2f1b072a9
peeyush0928 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-03-10 14:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (38.21%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -11,21 +11,39 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head == nullptr) return head;
-        ListNode* temp = head;
-        vector<int> arr;
-        while(temp != nullptr){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        // if(head == nullptr) return head;
+        // ListNode* temp = head;
+        // vector<int> arr;
+        // while(temp != nullptr){
+        //     arr.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // reverse(arr.begin() + left - 1, arr.begin() + right);
+        // temp = head;
+        // int index = 0;
+        // while(temp != nullptr){
+        //     temp->val = arr[index];
+        //     temp = temp->next;
+        //     index++;
+        // }
+        // return head;
+
+        // Optimal solution of that code using(one-pass, In-place)
+        if(!head || left == right) return head;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        for (int i = 0; i < left - 1; i++){
+            prev = prev->next;
         }
-        reverse(arr.begin() + left - 1, arr.begin() + right);
-        temp = head;
-        int index = 0;
-        while(temp != nullptr){
-            temp->val = arr[index];
-            temp = temp->next;
-            index++;
+        ListNode* curr = prev->next;
+        ListNode* nextNode = nullptr;
+        for (int i = 0; i < right - left; i++){
+            nextNode = curr->next;
+            curr->next = nextNode->next;
+            nextNode->next = prev->next;
+            prev->next = nextNode;
         }
-        return head;
+        return dummy.next;
     }
 };

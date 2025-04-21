c1cc6c7e70bd13b5d49f0291368b0250373004be
peeyush0928 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-04-21 18:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (43.54%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,33 +11,19 @@
  */
 class Solution {
 public:
-// that is not an optimal solution become in this code we are (Using extra space we are using result to store the element)
-    // void preorderHelper(TreeNode* root, vector<int>& result){
-    //     if(!root) return;
-    //     result.push_back(root->val);
-    //     preorderHelper(root->left, result);
-    //     preorderHelper(root->right, result);
-    // }
-    // vector<int> preorderTraversal(TreeNode* root) {
-    //     vector<int>result;
-    //     preorderHelper(root, result);
-    //     return result;
-    // }
-// Iterative Solution (Optimal - No Recursion, Uses Stack)
-    vector<int> preorderTraversal(TreeNode* root){
-        vector<int>result;
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
         if (!root) return result;
 
-        stack<TreeNode*> st;
+        stack<TreeNode* > st;
         st.push(root);
-
         while(!st.empty()){
-            TreeNode* node = st.top();
-            st.pop();
-            result.push_back(node->val);
+             TreeNode* current = st.top();
+        st.pop();
+        result.push_back(current->val);
 
-            if(node->right) st.push(node->right);
-            if(node->left) st.push(node->left);
+        if (current->right) st.push(current->right);
+        if (current->left) st.push(current->left);
         }
         return result;
     }

ebe694b291b414412dd1c7c1f32a19a1a05e44d2
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-05-28 22:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.80%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
+        if (n <= 2 ) return n;
         int one = 2;
         int two = 1;
         int allways = 0;
-
         for (int i = 3; i <= n; ++i){
             allways = one + two;
             two = one;

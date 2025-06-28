04871a86f6683f3b8372d58d6573ea28827abe7d
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-06-28 10:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (93.72%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // If you want to insert at the end
-        intervals.push_back(newInterval);
-        // If You want to insert at the start
-        // intervals.insert(intervals.begin(), newInterval);
-        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b){
-            return a[0] < b[0];
-        });
-        vector<vector<int>> result;
-        for(auto interval : intervals){
+        // that code is correct but it has two mistake
+        // firstmistake is that -> Note that you don't need to modify intervals in-place. You can make a new array and return it.
+        // secound mistake is that it has high time complexity O(n log n)
+
+        // // If you want to insert at the end
+        // intervals.push_back(newInterval);
+        // // If You want to insert at the start
+        // // intervals.insert(intervals.begin(), newInterval);
+        // sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b){
+        //     return a[0] < b[0];
+        // });
+        // vector<vector<int>> result;
+        // for(auto interval : intervals){
             
-            if(result.empty() || result.back()[1] < interval[0]){
-                result.push_back(interval);
-            }
-            else{
-                result.back()[1] = max(result.back()[1], interval[1]);
-            }
+        //     if(result.empty() || result.back()[1] < interval[0]){
+        //         result.push_back(interval);
+        //     }
+        //     else{
+        //         result.back()[1] = max(result.back()[1], interval[1]);
+        //     }
+        // }
+        // return result;
+
+        vector<vector<int>> res;
+        int i = 0, n = intervals.size();
+
+        while(i < n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i++]);
         }
-        return result;
+
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            ++i;
+        }
+        res.push_back(newInterval);
+
+        while(i < n) res.push_back(intervals[i++]);
+
+        return res;
     }
 };

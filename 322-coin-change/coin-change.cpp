dc16f417e63f90711a0321ccb576702e5c6b670a
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-06-29 14:12:37 +0530

    Time: 16 ms (88.74%) | Memory: 18 MB (63.86%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // this sis the greedy code but is not completely correct 
+
+
+        // sort(coins.begin(), coins.end());
+        // int count = 0;
+        // int i = coins.size() - 1;
+        // while(amount > 0 && i >= 0){
+        //     if(amount >= coins[i]){
+        //         int num = amount / coins[i];
+        //         count += num;
+        //         amount -= coins[i] * num;
+        //     }else{
+        //         i--;
+        //     }
+        // }
+        // return amount == 0 ? count : -1;
+
+        // correct code using DP
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; ++i){
+            for (int coin : coins) {
+                if (i >= coin){
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? - 1 : dp[amount];
+    }
+};

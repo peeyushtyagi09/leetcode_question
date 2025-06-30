098cabcfdf852a97dbafc0f2c807e1fdbcaa1db4
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-06-30 08:30:26 +0530

    Time: 14 ms (94.85%) | Memory: 17.9 MB (83.73%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -21,14 +21,13 @@ public:
         // correct code using DP
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
-
-        for (int i = 1; i <= amount; ++i){
-            for (int coin : coins) {
+        for (int i = 0; i <= amount; ++i){
+            for (int coin : coins){
                 if (i >= coin){
                     dp[i] = min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        return dp[amount] > amount ? - 1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };

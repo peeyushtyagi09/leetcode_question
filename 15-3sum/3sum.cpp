8d543d5faa2d8cd1bcaf61772c01997b6ae2fb1a
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-01 09:58:46 +0530

    Time: 43 ms (86.81%) | Memory: 29.2 MB (51.70%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,40 +4,31 @@ public:
         sort(nums.begin(), nums.end());
 
         vector<vector<int>> ans;
+
         const int n = nums.size();
         
-        for (int i = 0; i < n - 2; ++i){
+        for (int i = 0; i < n - 2; i++){
 
-            // ==== Skip Duplicates === 
+            // === Skip duplicates ====
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            // Only need to search if current value can still reach 0
             if (nums[i] > 0) break;
-
-            //  === Two Pointer === 
             int j = i + 1, k = n - 1;
 
-            while(j < k) {
+            while(j < k){
                 const long long sum = 1LL*nums[i] + nums[j] + nums[k];
 
                 if (sum == 0){
-                    // Found a triplet
                     ans.push_back({nums[i], nums[j], nums[k]});
 
-                    // ==== Skip duplicates for the secound element ==== 
-                    while (j < k && nums[j] == nums[j + 1]) ++j;
+                    while(j < k && nums[j] == nums[j + 1]) ++j;
 
-                    // ==== Skip duplicates for the third element ==== 
-                    while (j < k && nums[k] == nums[k - 1]) --k;
+                    while(j < k && nums[k] == nums[k - 1]) --k;
 
                     ++j;
                     --k;
-                }
-                else if (sum < 0){
-                    // need a larger sum
+                }else if(sum < 0){
                     ++j;
                 }else{
-                    // sum > 0 -> need a smaller sum
                     --k;
                 }
             }

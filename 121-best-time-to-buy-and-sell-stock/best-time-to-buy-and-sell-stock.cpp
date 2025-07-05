9e70c38338d829f0d6a096048d918e99d9d3e3cb
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-05 09:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (25.61%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,10 +5,10 @@ public:
         int best_buy = prices[0];
         int maxi = INT_MIN;
         for (int i = 1; i < prices.size(); i++){
-                if (prices[i] > best_buy){
-                    maxi = max(maxi, prices[i] - best_buy);
-                }
-                best_buy = min(best_buy, prices[i]);
+            if (prices[i] > best_buy){
+                maxi = max(maxi, prices[i] - best_buy);
+            }
+            best_buy = min(best_buy, prices[i]);
         }
         return (maxi == INT_MIN) ? 0 : maxi;
     }

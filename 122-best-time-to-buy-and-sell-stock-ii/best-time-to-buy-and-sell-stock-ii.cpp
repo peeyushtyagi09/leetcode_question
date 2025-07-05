284618f93927937c769ecf13db1303d30577cc84
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-05 10:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (49.03%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         // first using Greedy approach 
-        int profit = 0;
+        // int profit = 0;
+        // for (int i = 1; i < prices.size(); i++){
+        //     if (prices[i] > prices[i - 1]){
+        //         profit += prices[i] - prices[i - 1];
+        //     }
+        // }
+        // return profit;
+
+        // now using Dp 
+        int flat = 0;
+        int hold = -prices[0];
         for (int i = 1; i < prices.size(); i++){
-            if (prices[i] > prices[i - 1]){
-                profit += prices[i] - prices[i - 1];
-            }
+            int newflat = max(flat, hold + prices[i]);
+            int newhold = max(hold, flat - prices[i]);
+            flat = newflat;
+            hold = newhold;
         }
-        return profit;
+        return flat;
     }
 };

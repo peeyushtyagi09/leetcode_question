e22958837830932a9edf76a01dff95b136c92553
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-05 10:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 79.1 MB (99.43%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-          int buy1 = INT_MIN, sell1 = 0;
-        int buy2 = INT_MIN, sell2 = 0;
-
-        for (int price : prices) {
-            buy1  = std::max(buy1, -price);
-            sell1 = std::max(sell1, buy1 + price);
-            buy2  = std::max(buy2, sell1 - price);
-            sell2 = std::max(sell2, buy2 + price);
-        }
-
-        return sell2;
+        int buy1 = INT_MIN, sell1 = 0;
+         int buy2 = INT_MIN, sell2 = 0;
+         for (int price : prices){
+            buy1 = max(buy1, -price);
+            sell1 = max(sell1, buy1 + price);
+            buy2 = max(buy2, sell1 - price);
+            sell2 = max(sell2, buy2 + price);
+         }
+         return sell2;
     }
 };

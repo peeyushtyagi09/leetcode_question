8002ab61e92581cf4ffc274a9847ec9207a0f7d6
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-07 22:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (14.01%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,32 +11,42 @@
  */
 class Solution {
 public:
-    vector<vector<int>> check(TreeNode* root){
-        vector<vector<int>>result;
-        if (root == nullptr) return result;
+    // vector<vector<int>> check(TreeNode* root){
+    //     vector<vector<int>>result;
+    //     if (root == nullptr) return result;
 
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector<int> level;
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     while(!q.empty()){
+    //         int size = q.size();
+    //         vector<int> level;
 
-            for (int i = 0; i < size; i++){
-                TreeNode* current = q.front();
-                q.pop();
+    //         for (int i = 0; i < size; i++){
+    //             TreeNode* current = q.front();
+    //             q.pop();
 
-                level.push_back(current->val);
+    //             level.push_back(current->val);
 
-                if(current->left != nullptr) q.push(current->left);
-                if(current->right != nullptr) q.push(current->right);
-            }
-            result.push_back(level);
-        }
-        return result;
-    }
+    //             if(current->left != nullptr) q.push(current->left);
+    //             if(current->right != nullptr) q.push(current->right);
+    //         }
+    //         result.push_back(level);
+    //     }
+    //     return result;
+    // }
     int maxDepth(TreeNode* root) {
-       vector<vector<int>> res = check(root);
-        int ans = res.size();
-        return ans;
+    //    vector<vector<int>> res = check(root);
+    //     int ans = res.size();
+    //     return ans;
+
+
+    // that optimal solution of that code is using DFS - Recursion
+
+        if (root == nullptr) return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1 + max(leftDepth, rightDepth);
     }
 };

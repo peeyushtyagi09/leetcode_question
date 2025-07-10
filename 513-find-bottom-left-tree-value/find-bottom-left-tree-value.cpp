f3aace501d810c6f7dff018b734600d2bc3d2352
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-10 21:53:15 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (63.53%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -12,27 +12,48 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        vector<vector<int>> result;
-        if(root == nullptr) return 0;
+        // vector<vector<int>> result;
+        // if(root == nullptr) return 0;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // int n = 0;
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     vector<int> level;
+        //     for (int i = 0; i < size; i++){
+        //         TreeNode*  current = q.front();
+        //         q.pop();
+        //         level.push_back(current->val);
+
+        //         if(current->left != nullptr)q.push(current->left);
+        //         if(current->right != nullptr) q.push(current->right);
+        //     }
+        //     result.push_back(level);
+        //     n++;
+        // }
+        // int ans = 0;
+        // ans = result[n-1][0];
+        // return ans;
+
+        // optimal solution of that code 
         queue<TreeNode*> q;
         q.push(root);
-        int n = 0;
+        int leftmost = root->val;
+
         while(!q.empty()){
             int size = q.size();
-            vector<int> level;
-            for (int i = 0; i < size; i++){
-                TreeNode*  current = q.front();
-                q.pop();
-                level.push_back(current->val);
 
-                if(current->left != nullptr)q.push(current->left);
-                if(current->right != nullptr) q.push(current->right);
+            // First node of the level is leftmost
+            leftmost = q.front()->val;
+
+            for (int i = 0; i < size; i++){
+                TreeNode * curr = q.front();
+                q.pop();
+
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
-            result.push_back(level);
-            n++;
         }
-        int ans = 0;
-        ans = result[n-1][0];
-        return ans;
+        return leftmost;
     }
 };

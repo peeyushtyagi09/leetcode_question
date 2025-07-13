f3113c691998f49e0ec2806cba47727a911c65dc
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-13 08:55:20 +0530

    Time: 1 ms (21.70%) | Memory: 19.6 MB (67.95%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -12,8 +12,33 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (!root) return 0;
+        // if (!root) return 0;
 
+        // queue<pair<TreeNode*, unsigned long long>> q;
+        // q.push({root, 0});
+        // int maxWidth = 0;
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     unsigned long long minIndex = q.front().second;
+        //     unsigned long long first, last;
+
+        //     for (int i = 0; i < size; ++i){
+        //         auto [node, index] = q.front();
+        //         q.pop();
+        //         index -= minIndex;
+
+        //         if (i == 0) first = index;
+        //         if (i == size - 1) last = index;
+
+        //         if (node->left) q.push({node->left, 2*index});
+        //         if (node->right) q.push({node->right, 2 * index + 1});
+        //     }
+
+        //     maxWidth = max(maxWidth, (int)(last - first + 1));
+        // }
+        // return maxWidth;
+
+        if(root == nullptr) return 0;
         queue<pair<TreeNode*, unsigned long long>> q;
         q.push({root, 0});
         int maxWidth = 0;
@@ -21,9 +46,8 @@ public:
             int size = q.size();
             unsigned long long minIndex = q.front().second;
             unsigned long long first, last;
-
-            for (int i = 0; i < size; ++i){
-                auto [node, index] = q.front();
+            for (int i = 0; i < size; i++){
+              auto [node, index] = q.front();
                 q.pop();
                 index -= minIndex;
 
@@ -37,6 +61,5 @@ public:
             maxWidth = max(maxWidth, (int)(last - first + 1));
         }
         return maxWidth;
-
     }
 };

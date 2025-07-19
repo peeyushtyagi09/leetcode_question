f2c5e5e4f8448b134e27470514432e3097dc1e47
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-19 15:26:04 +0530

    Time: 75 ms (39.27%) | Memory: 196.6 MB (63.48%) - LeetSync

---
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void inorder(TreeNode* root, vector<int>& sorted){
+    if(!root) return;
+    inorder(root->left, sorted);
+    sorted.push_back(root->val);
+    inorder(root->right, sorted);
+}
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
+        vector<int> sorted;
+        inorder(root,sorted);
+
+        vector<vector<int>> ans;
+        for( int q : queries){
+            int low = 0, high = sorted.size() - 1;
+            int floor = -1, ceil = -1;
+
+     // Find floor (<= q)
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (sorted[mid] <= q) {
+                    floor = sorted[mid];
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+            low = 0, high = sorted.size() - 1;
+            // Find ceil (>= q)
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (sorted[mid] >= q) {
+                    ceil = sorted[mid];
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            ans.push_back({floor, ceil});
+        }
+
+        return ans;
+    }
+};

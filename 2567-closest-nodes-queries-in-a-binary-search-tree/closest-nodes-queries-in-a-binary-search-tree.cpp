7bba4393c24d2f515ffc6342d443e4d4814caeb1
peeyushtyagi09 <149046835+peeyushtyagi09@users.noreply.github.com>
2025-07-20 14:23:37 +0530

    Time: 73 ms (43.87%) | Memory: 196.7 MB (47.81%) - LeetSync

--- 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -12,46 +12,42 @@
 class Solution {
 public:
 void inorder(TreeNode* root, vector<int>& sorted){
-    if(!root) return;
+    if (root == nullptr) return;
     inorder(root->left, sorted);
     sorted.push_back(root->val);
     inorder(root->right, sorted);
 }
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
         vector<int> sorted;
-        inorder(root,sorted);
+        inorder(root, sorted);
 
-        vector<vector<int>> ans;
-        for( int q : queries){
-            int low = 0, high = sorted.size() - 1;
+        vector<vector<int>>ans;
+        for (int q : queries){
             int floor = -1, ceil = -1;
 
-     // Find floor (<= q)
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (sorted[mid] <= q) {
+            
+            int low = 0, high = sorted.size() - 1;
+            while(low <= high){
+                int mid = low + (high - low) / 2;
+                if (sorted[mid] <= q){
                     floor = sorted[mid];
                     low = mid + 1;
-                } else {
+                }else{
                     high = mid - 1;
                 }
             }
-
             low = 0, high = sorted.size() - 1;
-            // Find ceil (>= q)
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (sorted[mid] >= q) {
+            while (low <= high){
+                int mid = low + (high - low) / 2;
+                if (sorted[mid] >= q){
                     ceil = sorted[mid];
                     high = mid - 1;
-                } else {
+                }else{
                     low = mid + 1;
                 }
             }
-
             ans.push_back({floor, ceil});
         }
-
         return ans;
     }
 };
